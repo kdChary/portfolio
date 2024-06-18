2a22fdb6f2e15b3c767cc150e8c2a65a373f33a8
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-18 14:02:10 +0530

    Root router implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useMode, ThemeContext } from './Theme';
+import { ThemeProvider } from '@emotion/react';
 
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const App = () => {
-	return <div>lets get started</div>;
+	const [theme, colorMode] = useMode();
+
+	const router = createBrowserRouter([
+		{ path: '/', element: <div>Hello</div>, children: [] },
+	]);
+	return (
+		<ThemeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<RouterProvider router={router} />
+			</ThemeProvider>
+		</ThemeContext.Provider>
+	);
 };
 
 export default App;
