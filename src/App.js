20b98edabcebd4fec371fb1d9b0652d8eb156d37
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-18 14:39:01 +0530

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { useMode, ThemeContext } from './Theme';
 import { ThemeProvider } from '@emotion/react';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useMode, ThemeContext } from './Theme';
+import Home from './Pages/home/Home';
+import About from './Pages/about/About';
+import Skills from './Pages/skills/Skills';
+import Projects from './Pages/projects/Projects';
+import Contacts from './Pages/contacts/Contacts';
 
 const App = () => {
 	const [theme, colorMode] = useMode();
 
 	const router = createBrowserRouter([
-		{ path: '/', element: <div>Hello</div>, children: [] },
+		{
+			path: '/',
+			element: (
+				<>
+					<div>Hello</div>
+					<Outlet />
+				</>
+			),
+			children: [
+				{ path: '/', element: <Home /> },
+				{ path: '/about', element: <About /> },
+				{ path: '/skills', element: <Skills /> },
+				{ path: '/projects', element: <Projects /> },
+				{ path: '/contacts', element: <Contacts /> },
+			],
+		},
 	]);
+
 	return (
 		<ThemeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
